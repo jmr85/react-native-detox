14715e10f635c2019bd8b849f1ebc2b5f0225f6f
Juan Martin Ruiz <juan.martin.ruiz@gmail.com>
2019-11-01 12:43:13 -0300

    add ABM User project

--- app.js
+++ app.js
@@ -1,49 +1,12 @@
 import React, {Component} from 'react';
-import { AppRegistry, StyleSheet, Text, View } from 'react-native';
-import {createAppContainer} from 'react-navigation';
-import { createStackNavigator } from 'react-navigation-stack'
-import ProductScreen from './components/ProductScreen';
-import ProductDetailsScreen from './components/ProductDetailsScreen';
-import ProductAddScreen from './components/ProductAddScreen';
-import ProductEditScreen from './components/ProductEditScreen';
+import { AppRegistry } from 'react-native';
+import AppNavigator from './AppNavigator';
 
-const RootStack = createStackNavigator(
-  {
-    Product: ProductScreen,
-    ProductDetails: ProductDetailsScreen,
-    AddProduct: ProductAddScreen,
-    EditProduct: ProductEditScreen,
-  },
-  {
-    initialRouteName: 'Product',
-    navigationOptions: {
-      headerStyle: {
-        backgroundColor: '#777777',
-      },
-      headerTintColor: '#fff',
-      headerTitleStyle: {
-        fontWeight: 'bold',
-      },
-    },
-  },
-);
-
-const RootContainer = createAppContainer(RootStack);
 
 class example extends Component {
   render() {
-    return <RootContainer style={styles.container} />;
+    return <AppNavigator />;
   }
 }
-
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
 
 AppRegistry.registerComponent('example', () => example);
